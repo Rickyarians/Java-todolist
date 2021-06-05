21c81f398de0b6e4424d4b861f8587ccedb40082
rickyastra <ricky.ariansyah@menara-astra.co.id>
2021-06-05 19:40:38 +0700

    create model dan method

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -1,8 +1,32 @@
 public class AplikasiTodolist {
 
+    public static String[] model = new String[10];
+
     public static void main(String[] args) {
 
 
+
+    }
+
+
+    /*
+    * Menampilkan Todo List
+    * */
+    public static void showTodoList(){
+
+    }
+
+    /*
+    * Meanambah todo kedalam list
+    * */
+    public static void addTodoList(){
+
+    }
+
+
+    /*
+    * Menghapus todo list*/
+    public static void removeTodoList(){
 
     }
 }
